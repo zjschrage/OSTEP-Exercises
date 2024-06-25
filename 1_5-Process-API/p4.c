f7292dd375daa00ffe8d3947b54ac7e08395cded
Zack Schrage <zjschrage@gmail.com>
2024-06-25 01:04:50 -0500

    Problem 1.5.4 Input Switch Exec Flavor

--- 1_5-Process-API/p4.c
+++ 1_5-Process-API/p4.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+
 #include "common.h"
 
 /*
@@ -8,6 +10,15 @@
     you think there are so many variants of the same basic call?
 */
 
+typedef enum ExecFlavor
+{
+    L, LP, LE,
+    V, VP, VE
+
+} ExecFlavor;
+
+static ExecFlavor exec_flavor = L;
+
 void child_function()
 {
     char* path = "/bin/ls";
@@ -15,14 +26,15 @@ void child_function()
     char* args[2] = {program, NULL};
     char* env[2] = {"HOME=/home", "MY_FLAG=flag"};
 
-    //Only the first will execute since the first exec will overwrite program memory
-    //Rearrange these and recompile to change the exec flavor
-    execl(path, args[0], args[1]);
-    execlp(program, args[0], args[1]);
-    execle(path, args[0], args[1], env);
-    execv(path, args);
-    execvp(program, args);
-    execve(path, args, env);
+    switch (exec_flavor)
+    {
+        case L:  execl(path, args[0], args[1]);        break;
+        case LP: execlp(program, args[0], args[1]);    break;
+        case LE: execle(path, args[0], args[1], env);  break;
+        case V:  execv(path, args);                    break;
+        case VP: execvp(program, args);                break;
+        case VE: execve(path, args, env);              break;
+    }
 }
 
 void parent_function()
@@ -30,8 +42,55 @@ void parent_function()
     wait(NULL);
 }
 
+int chars_match(char test, char target)
+{
+    return (test == target || test == (char)(toupper(target)));
+}
+
+void handle_input(int argc, char* argv[])
+{
+    const char list = 'l';
+    const char vector = 'v';
+    const char prog = 'p';
+    const char env = 'e';
+
+    int isList = 1;
+    int isName = 0;
+    int hasEnv = 0;
+
+    if (argc < 2)
+    {
+        printf("Missing Args: lvpe\n");
+        exit(1);
+    }
+
+    char* arg = argv[1];
+    for (int i = 0; i < strlen(arg); i++)
+    {
+        if (chars_match(arg[i], list))         isList = 1;
+        else if (chars_match(arg[i], vector))  isList = 0;
+        else if (chars_match(arg[i], prog))    isName = 1;
+        else if (chars_match(arg[i], env))     hasEnv = 1;
+    }
+
+    if (isList)
+    {
+        if (isName) exec_flavor = LP;
+        else if (hasEnv) exec_flavor = LE;
+        else exec_flavor = L;
+    }
+    else
+    {
+        if (isName) exec_flavor = VP;
+        else if (hasEnv) exec_flavor = VE;
+        else exec_flavor = V;
+    }
+    printf("Running flavor %d\n", exec_flavor);
+}
+
 int main(int argc, char* argv[])
 {
     supress_output = 1;
+    handle_input(argc, argv);
     zfork(&child_function, &parent_function);
 }
